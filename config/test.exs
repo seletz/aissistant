0bde30eb23197f749dead34f6fb3a86ee0db04b1
Stefan Eletzhofer <stefan.eletzhofer@nexiles.com>
2023-03-02 13:33:25 +0100

    Fix test environment

--- config/test.exs
+++ config/test.exs
@@ -9,8 +9,8 @@ config :bcrypt_elixir, :log_rounds, 1
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :aissistant, Aissistant.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "phoenix",
+  password: "phoenix",
   hostname: "localhost",
   database: "aissistant_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
